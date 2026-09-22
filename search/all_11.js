4f1d4e31bb046f922cf7189a6131d6af61eff95f
pbosetti <paolo.bosetti@me.com>
2026-09-22 10:30:43 +0000

    Deploying to gh-pages from @ pbosetti/mads_plugin@5b3c934aadb93d78247ba34108ca548c39bfbfb4 🚀

--- search/all_11.js
+++ search/all_11.js
@@ -1,7 +1,9 @@
 var searchData=
 [
   ['templates_0',['Templates',['../index.html#autotoc_md1',1,'']]],
-  ['timed_5fout_1',['timed_out',['../classSerialPort.html#a8a2579dddcd190e581e79247b5915bf9',1,'SerialPort']]],
-  ['timeout_2',['timeout',['../structSerialPort_1_1Config.html#ad2351d7223a86904b45868481fcd7833',1,'SerialPort::Config::timeout'],['../classSerialPort.html#a5cfd5d6ae46b4068a0e1f083dc0910a9',1,'SerialPort::timeout()']]],
-  ['toconsole_3',['ToConsole',['../classToConsole.html',1,'']]]
+  ['the_20tt_20serialport_20tt_20class_1',['The &lt;tt&gt;SerialPort&lt;/tt&gt; class',['..//home/runner/work/mads_plugin/mads_plugin/README.md#autotoc_md17',1,'']]],
+  ['timed_5fout_2',['timed_out',['../classSerialPort.html#a8a2579dddcd190e581e79247b5915bf9',1,'SerialPort']]],
+  ['timeout_3',['timeout',['../structSerialPort_1_1Config.html#ad2351d7223a86904b45868481fcd7833',1,'SerialPort::Config::timeout'],['../classSerialPort.html#a5cfd5d6ae46b4068a0e1f083dc0910a9',1,'SerialPort::timeout()']]],
+  ['toconsole_4',['ToConsole',['../classToConsole.html',1,'']]],
+  ['tt_20serialport_20tt_20class_5',['The &lt;tt&gt;SerialPort&lt;/tt&gt; class',['..//home/runner/work/mads_plugin/mads_plugin/README.md#autotoc_md17',1,'']]]
 ];
--- search/all_a.js
+++ search/all_a.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['last_5ferror_0',['last_error',['../classSerialPort.html#a28789658b41ae486a170bbdfb25d5823',1,'SerialPort']]],
-  ['load_5fdata_1',['load_data',['../classFilter.html#a7c4dfbfb5125a96e6bc04ce09da169aa',1,'Filter::load_data()'],['../classSink.html#af0b33c220b3812385402e1fadb3ea9c2',1,'Sink::load_data()']]]
+  ['license_1',['License',['..//home/runner/work/mads_plugin/mads_plugin/README.md#autotoc_md18',1,'']]],
+  ['load_5fdata_2',['load_data',['../classFilter.html#a7c4dfbfb5125a96e6bc04ce09da169aa',1,'Filter::load_data()'],['../classSink.html#af0b33c220b3812385402e1fadb3ea9c2',1,'Sink::load_data()']]]
 ];
